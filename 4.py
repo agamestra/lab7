95d1df046fe19c97c7606d05ee6f3fc4d11c69e2
agamestra <shyshygon@ya.ru>
2015-10-13 11:23:01 +0300

    Work "Grafics"

--- 4.py
+++ 4.py
@@ -3,23 +3,15 @@ import matplotlib.pyplot as plt
 import pylab
 from matplotlib import mlab
 import math
-x=np.arange(-10,10.01,0.01)
-tmin=-20
-tmax=20
-amin=-20
-amax=20
-x = np.sin(t+a)
-y = np.cos(2*t)
-
-dx = 0.01
-dy=0.01
-xlist = mlab.frange (tmin, tmax, dx)
-ylist = mlab.frange (tmin, tmax, dy)
+tlist = mlab.frange (-10, 10, 0.005)
+alist = mlab.frange (-10, 10, 0.005)
 
 pylab.ion()
 
-for n in range (50):
-        ylist = [math.sin (x + n / 2.0) for x in xlist]
+for n in range (500):
+        a=n/20
+        xlist = [np.sin(t+a) for t in tlist]
+        ylist = [np.cos(2*t) for t in tlist]
         pylab.clf()
         pylab.plot (xlist, ylist)
         pylab.draw()
